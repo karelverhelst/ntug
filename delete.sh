725db6e5753bcbf80bd15942b536046b476015da
karelverhelst <karel.verhelst@netapp.com>
2022-11-23 12:25:32 +0100

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -2,6 +2,7 @@ sudo umount /mnt/web
 sudo umount /mnt/mariadb
 kubectl delete all --all -n ntug
 kubectl delete pvc --all -n ntug
+cd /home
 sudo rm -R /mnt/web
 sudo rm -R /mnt/mariadb
 sudo rm -R /home/user/ntug
