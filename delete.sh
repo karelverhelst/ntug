f3dd8bd46d5fc1f6a3f6a1f81cf99e5211da9193
karelverhelst <karel.verhelst@netapp.com>
2022-11-23 12:07:02 +0100

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,7 @@
+sudo umount /mnt/web
+sudo umount /mnt/mariadb
+kubectl delete all --all -n ntug
+kubectl delete pvc --all -n ntug
+sudo rm -R /mnt/web
+sudo rm -R /mnt/mariadb
+sudo rm -R /home/user/ntug
