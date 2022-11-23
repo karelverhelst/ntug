08dbcce65e148ec646f1858bded763fbda3e2d0a
karelverhelst <karel.verhelst@netapp.com>
2022-11-23 11:30:10 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
-alias cluster1="ssh admin@192.168.0.101"
-alias cluster3="ssh admin@192.168.0.103"
-alias k=kubectl
-
 sudo apt install -y nfs-common
 sudo apt install -y mysql-client
-
 
 rke1
 
@@ -15,8 +10,8 @@ kubectl apply -f pvc-mariadb.yaml -n ntug
 web=$(kubectl get pvc -n ntug | grep web | awk {'print$3'} | grep -o '....$')
 mariadb=$(kubectl get pvc -n ntug | grep mariadb | awk {'print$3'} | grep -o '....$')
 
-cluster1 vol show | grep $web > web.txt
-cluster1 vol show | grep $mariadb > mariadb.txt
+ssh admin@192.168.30.101 vol show | grep $web > web.txt
+ssh admin@192.168.30.101 vol show | grep $mariadb > mariadb.txt
 
 volweb=$(cat web.txt | awk{'print$2'} )
 volmariadb=$(cat mariadb.txt | awk {'print$2'} )
@@ -25,9 +20,6 @@ sudo mkdir /mnt/web
 sudo mount 192.168.0.131:/$volweb /mnt/web
 sudo mkdir /mnt/mariadb
 sudo mount 192.168.0.131:/$volmariadb /mnt/mariadb
-
-sudo cp ntugmariadb.tgz /mnt/mariadb
-sudo cp ntugweb.tgz /mnt/web
 
 sudo tar -xzvf ntug.tgz
 
